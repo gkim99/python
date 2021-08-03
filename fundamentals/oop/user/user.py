0cdbce3e0551b672bafc76636224d6cc93dddcfa
gkim99 <gracekim092@gmail.com>
2021-08-03 14:37:41 -0700

    third commit

--- fundamentals/oop/user/user.py
+++ fundamentals/oop/user/user.py
@@ -13,12 +13,14 @@ class User:
 
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: {self.account_balance}")
+        return self
 
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
         self.display_user_balance()
         other_user.display_user_balance()
+        return self
     
 rawr = User("Rawr")
 moo = User("Moo")
