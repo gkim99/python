d784765fdc6d46779299aed4c8d48671e624c96f
gkim99 <gracekim092@gmail.com>
2021-08-02 23:59:12 -0700

    second commit

--- fundamentals/fundamentals/functions_intermediate_i/functions_intermediate_i.py
+++ fundamentals/fundamentals/functions_intermediate_i/functions_intermediate_i.py
@@ -12,18 +12,27 @@ z = [ {'x': 10, 'y': 20} ]
 
 x[1][0] = 15
 students[0]["last_name"] = "Bryant"
-sports_directory["soccer"] = ["Andres", "Ronaldo", "Rooney"]
+sports_directory["soccer"][0] = "Andres"
 z[0]["y"] = 30
 
 #2. Iterate Through a List of Dictionaries
 def iterateDictionary(some_list):
-    for i in some_list:
-        for k,v in i.items():
-            return k, "-", v
+    for i in range(0,len(some_list)):
+        output = ""
+        for k,v in some_list[i].items():
+            output += f"{k} - {v}, "
+        print(output)
 
 #3. Get Values From a List of Dictionaries
 def iterateDictionary2(key_name, some_list):
+    for i in range(0,len(some_list)):
+        for k,v in some_list[i].items():
+            if k == key_name:
+                print(v)
 
 #4. Iterate Through a Dictionary with List Values
 def printInfo(some_dict):
-    
+    for k,v in some_dict.items():
+        print(f"{len(v)} {k.upper()}")
+        for i in range(0,len(v)):
+            print(v[i])
