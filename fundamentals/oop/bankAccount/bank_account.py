c48695cf6b8991a2b9eb43e62752d859372058dc
gkim99 <gracekim092@gmail.com>
2021-08-03 16:21:16 -0700

    second commit

--- fundamentals/oop/bankAccount/bank_account.py
+++ fundamentals/oop/bankAccount/bank_account.py
@@ -1,14 +1,20 @@
 class BankAccount:
+    accounts = []
     def __init__(self, int_rate, balance): 
         self.int_rate = int_rate
         self.balance = balance
+        BankAccount.accounts.append(self)
 
     def deposit(self, amount): 
         self.balance += amount
         return self
 
     def withdraw(self, amount):
-        self.balance -= amount
+        if (self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
         return self
 
     def display_account_info(self):
@@ -16,11 +22,19 @@ class BankAccount:
         return self 
 
     def yield_interest(self):
-        self.balance = self.balance * self.int_rate + self.balance
+        if (self.balance > 0):
+            self.balance += (self.balance * self.int_rate)
         return self
 
-rawr = BankAccount(0.01, 200)
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+rawr = BankAccount(0.01, 1000)
 moo = BankAccount(0.02, 800)
 
 rawr.deposit(300).deposit(500).deposit(100).withdraw(300).yield_interest().display_account_info()
 moo.deposit(300).deposit(700).withdraw(100).withdraw(200).withdraw(150).withdraw(90).yield_interest().display_account_info()
+
+BankAccount.print_all_accounts()
