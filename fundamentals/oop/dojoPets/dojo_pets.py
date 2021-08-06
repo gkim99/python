9f39694211a4bb4d080d6924259cfddcaa843665
gkim99 <gracekim092@gmail.com>
2021-08-06 14:48:27 -0700

    initial commit

--- fundamentals/oop/dojoPets/dojo_pets.py
+++ fundamentals/oop/dojoPets/dojo_pets.py
@@ -1,29 +1,52 @@
 class Ninja:
-    def __init__(self, first_name, last_name, pet, treats, pet_food):
+    def __init__(self, first_name, last_name, treats, pet_food, pet):
         self.first_name = first_name
         self.last_name = last_name
         self.pet = pet
         self.treats = treats
         self.pet_food = pet_food
 
+    def walk(self):
+        self.pet.play()
+        return self
+
+    def feed(self):
+        self.pet.eat()
+        return self
+
+    def bathe(self):
+        self.pet.noise()
+        return self
+
 
 class Pet:
-    def __init__(name, type, tricks):
-        name = name
-        type = type
-        tricks = tricks
-        health = 0
-        energy = 0
+    def __init__(self, name, type, tricks, noise):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = 50
+        self.energy = 50
+        self.noise = noise
 
-    def sleep(energy):
-        energy += 25
+    def sleep(self):
+        self.energy += 25
+        return self
 
-    def eat(energy, health):
-        energy += 5
-        health += 10
+    def eat(self):
+        self.energy += 5
+        self.health += 10
+        return self
 
-    def play(health):
-        health += 5
+    def play(self):
+        self.health += 5
+        return self
 
-    def noise(noise):
-        print(noise)
+    def noise(self):
+        print(self.noise)
+        return self
+
+rubby = Pet("ear", "bunny", "bark", "woof")
+
+hello = Ninja("hello", "bye", "carrtots", "nibble", rubby)
+
+hello.feed().walk().bathe()
